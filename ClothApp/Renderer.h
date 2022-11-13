482f795f9c560274dee73d5c5d7dde59433f20df
Soumik Dutta <shalearkane@gmail.com>
2022-11-14 00:43:50 +0530

    reformatted code
    Signed-off-by: Soumik Dutta <shalearkane@gmail.com>

--- ClothApp/Renderer.h
+++ ClothApp/Renderer.h
@@ -1,22 +1,21 @@
 #pragma once
-#include <glm/gtc/matrix_transform.hpp>
 #include "Shader.h"
-
+#include <glm/gtc/matrix_transform.hpp>
 
 class Renderer {
-protected:
-	GLProgram* program;
-	ProgramInput* input;
-	unsigned int n_elements;
+  protected:
+    GLProgram *program;
+    ProgramInput *input;
+    unsigned int n_elements;
 
-public:
-	Renderer();
+  public:
+    Renderer();
 
-	void setProgram(GLProgram* program);
-	void setProgramInput(ProgramInput* input);
-	void setModelview(const glm::mat4& mv);
-	void setProjection(const glm::mat4& p);
-	void setElementCount(unsigned int n_elements);
+    void setProgram(GLProgram *program);
+    void setProgramInput(ProgramInput *input);
+    void setModelview(const glm::mat4 &mv);
+    void setProjection(const glm::mat4 &p);
+    void setElementCount(unsigned int n_elements);
 
-	void draw();
+    void draw();
 };
--- ClothApp/UserInteraction.h
+++ ClothApp/UserInteraction.h
@@ -5,31 +5,33 @@
 #include "Renderer.h"
 
 class UserInteraction {
-protected:
-	typedef glm::vec3 vec3;
-	typedef std::vector<unsigned char> color;
+  protected:
+    typedef glm::vec3 vec3;
+    typedef std::vector<unsigned char> color;
 
-	int i; // index of fixed point
-	float* vbuff; // vertex buffer
-	CgPointFixNode* fixer; // point fixer
-	Renderer* renderer; // pick shader renderer
-	virtual int colorToIndex(color c) const = 0;
+    int i;                 // index of fixed point
+    float *vbuff;          // vertex buffer
+    CgPointFixNode *fixer; // point fixer
+    Renderer *renderer;    // pick shader renderer
+    virtual int colorToIndex(color c) const = 0;
 
-public:
-	UserInteraction(Renderer* renderer, CgPointFixNode* fixer, float* vbuff);
+  public:
+    UserInteraction(Renderer *renderer, CgPointFixNode *fixer, float *vbuff);
 
-	void setModelview(const glm::mat4& mv);
-	void setProjection(const glm::mat4& p);
+    void setModelview(const glm::mat4 &mv);
+    void setProjection(const glm::mat4 &p);
 
-	void grabPoint(int mouse_x, int mouse_y); // grab point with color c
-	void movePoint(vec3 v); // move grabbed point along mouse
-	void releasePoint(); // release grabbed point;
+    void grabPoint(int mouse_x, int mouse_y); // grab point with color c
+    void movePoint(vec3 v);                   // move grabbed point along mouse
+    void releasePoint();                      // release grabbed point;
 };
 
 class GridMeshUI : public UserInteraction {
-protected:
-	const unsigned int n; // grid width
-	virtual int colorToIndex(color c) const;
-public:
-	GridMeshUI(Renderer* renderer, CgPointFixNode* fixer, float* vbuff, unsigned int n);
+  protected:
+    const unsigned int n; // grid width
+    virtual int colorToIndex(color c) const;
+
+  public:
+    GridMeshUI(Renderer *renderer, CgPointFixNode *fixer, float *vbuff,
+               unsigned int n);
 };
